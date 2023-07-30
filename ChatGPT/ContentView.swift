afb75566e0752cefc9fc4ffbaf7dc0b5aa2b82ac
Jaka Maver <jm18457@gmail.com>
2023-07-30 11:56:41 +0200

    Automatically open GPT v4

--- ChatGPT/ContentView.swift
+++ ChatGPT/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             if loadWebView {
-                WebView(url: URL(string: "https://chat.openai.com")!)
+                WebView(url: URL(string: "https://chat.openai.com?model=gpt-4")!)
                     .frame(minWidth: 200, maxWidth: .infinity, minHeight: 200, maxHeight: .infinity)
             }
         }
